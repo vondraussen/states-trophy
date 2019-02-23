c9f152ea05ba54d537fd57488f8c502bc6b8f65a
Andy <hempel.andy@gmail.com>
2019-02-23 16:44:44 -0500

    Fix page title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'States-Trophy' });
 });
 
 module.exports = router;
